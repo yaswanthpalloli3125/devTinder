07d8d818d91a0aecbdf018585bbb54b77b466a2b
Yaswanth <palloliyaswanth@gmail.com>
2025-12-15 23:08:53 +0530

    playing get post delete

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 const app = express();
 
-app.use("/home", (req,res)=>{
-   res.send("Hello from home");
+app.get("/user",(req,res)=>{
+   res.send({firstName:"yaswanth",lastName:"Palloli"})
 })
-
+app.post("/user",(req,res)=>{
+    res.send("user created successfully");
+ })
+ app.delete("/user",(req,res)=>{
+    res.send("user deleted successfully");
+ })
 app.use("/test", (req,res)=>{
     res.send("hello from test");
 })
+
 
 app.listen(7777,()=>{
     console.log("server is running on port 7777")
