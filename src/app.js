cba072e2e229f61bdffc800747a128a8beeda9ab
Yaswanth <palloliyaswanth@gmail.com>
2025-12-18 12:12:12 +0530

    dynamic signup

--- src/app.js
+++ src/app.js
@@ -1,33 +1,26 @@
 const express = require("express");
-const connectDB = require("./databse")
-const {adminAuth,userAuth} = require("./middleWares/auth")
-const {User} = require("./models/user");
+const connectDB = require("./databse");
+const { adminAuth, userAuth } = require("./middleWares/auth");
+const { User } = require("./models/user");
 const app = express();
 
-app.post("/signup", async (rq,res,next)=>{
-    const newUser = new User({
-        firstName:"Yaswanth",
-        lastName:"Palloli",
-        age:25,
-        gender:"Male"
-    })
-   try{
-       await newUser.save();
+app.use(express.json());
+
+app.post("/signup", async (req, res, next) => {
+  const newUser = new User(req.body);
+  try {
+    await newUser.save();
     res.send("user added succesfully");
-   }catch(err){
-       res.status(400).send("error in saving user data"+ err.message);
-   }
-})
-
-
-connectDB().then(()=>{
-    console.log("connection established to database");
-    app.listen(7777,()=>{
-    console.log("server is running on port 7777");
+  } catch (err) {
+    res.status(400).send("error in saving user data" + err.message);
+  }
 });
 
-}).catch(err=>console.log("connection cannot established to database"));
-
-
-
-
+connectDB()
+  .then(() => {
+    console.log("connection established to database");
+    app.listen(7777, () => {
+      console.log("server is running on port 7777");
+    });
+  })
+  .catch((err) => console.log("connection cannot established to database"));
