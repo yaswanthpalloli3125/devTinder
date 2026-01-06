0b0397322d022adecb2ba8f84641d370ad3df3c2
Yaswanth <palloliyaswanth@gmail.com>
2026-01-06 23:02:51 +0530

    ses package and email config

---
+++ src/utils/sendEmail.js
@@ -0,0 +1,60 @@
+const { SendEmailCommand } = require("@aws-sdk/client-ses");
+const { sesClient } = require("./libs/sesClient.js");
+
+const createSendEmailCommand = (toAddress, fromAddress) => {
+  return new SendEmailCommand({
+    Destination: {
+      /* required */
+      CcAddresses: [
+        /* more items */
+      ],
+      ToAddresses: [
+        toAddress,
+        /* more To-email addresses */
+      ],
+    },
+    Message: {
+      /* required */
+      Body: {
+        /* required */
+        Html: {
+          Charset: "UTF-8",
+          Data: "HTML_FORMAT_BODY",
+        },
+        Text: {
+          Charset: "UTF-8",
+          Data: "TEXT_FORMAT_BODY",
+        },
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: "EMAIL_SUBJECT",
+      },
+    },
+    Source: fromAddress,
+    ReplyToAddresses: [
+      /* more items */
+    ],
+  });
+};
+
+const run = async () => {
+  const sendEmailCommand = createSendEmailCommand(
+    "palloliyaswanth@gmail.com",
+    "yash@meetme4u.com",
+  );
+
+  try {
+    return await sesClient.send(sendEmailCommand);
+  } catch (caught) {
+    if (caught instanceof Error && caught.name === "MessageRejected") {
+      /** @type { import('@aws-sdk/client-ses').MessageRejected} */
+      const messageRejectedError = caught;
+      return messageRejectedError;
+    }
+    throw caught;
+  }
+};
+
+// snippet-end:[ses.JavaScript.email.sendEmailV3]
+module.exports = { run };
---
+++ src/utils/sesClient.js
@@ -0,0 +1,10 @@
+const { SESClient } = require("@aws-sdk/client-ses");
+// Set the AWS Region.
+const REGION = "us-east-1";
+// Create SES service object.
+const sesClient = new SESClient({ region: REGION,credentials:{
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SECRET_KEY
+} });
+module.exports = { sesClient };
+// snippet-end:[ses.JavaScript.createclientv3]
